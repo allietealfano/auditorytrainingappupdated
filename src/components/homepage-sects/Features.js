d2bad5032efcbbc66dbb227379019279421b1986
El <elton.lucien1@gmail.com>
2022-02-07 11:11:40 -0500

    landing-page
    header, features, and missions sections

---
+++ src/components/homepage-sects/Features.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+import "./sections.css";
+import "./features.css";
+
+function Features() {
+  //This array will likely be handled by a database
+  //It's a placeholder that should be deleted
+
+  const featuresArr = [
+    {
+      title: "Key Feature 1",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias sint quos? Accusantium a fugiat porro reiciendis saepe quibusdam debitis ducimus.",
+      pic: "../assets/images/test-pc.jpg",
+    },
+    {
+      title: "Key Feature 2",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias sint quos? Accusantium a fugiat porro reiciendis saepe quibusdam debitis ducimus.",
+      pic: "../assets/images/test-pc.jpg",
+    },
+    {
+      title: "Key Feature 3",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias sint quos? Accusantium a fugiat porro reiciendis saepe quibusdam debitis ducimus.",
+      pic: "../assets/images/test-pc.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      <section className="section" id="section--1">
+        <div className="section__title">
+          <h2 className="section__description">Features</h2>
+          <h3 className="section__header">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias
+          </h3>
+        </div>
+
+        <div className="feature__cards">
+          <div className="feature__card">
+            <h5 className="card__header">{featuresArr[0].title}</h5>
+            <p>{featuresArr[0].detail}</p>
+            <img
+              src={require("../../assets/images/test-pc.jpg")}
+              alt="Computer"
+              className="card__img"
+            />
+          </div>
+          <div className="feature__card">
+            <h5 className="card__header">{featuresArr[1].title}</h5>
+            <p>{featuresArr[1].detail}</p>
+            <img
+              src={require("../../assets/images/test-pc.jpg")}
+              alt="Computer"
+              className="card__img"
+            />
+          </div>
+          <div className="feature__card">
+            <h5 className="card__header">{featuresArr[2].title}</h5>
+            <p>{featuresArr[2].detail}</p>
+            <img
+              src={require("../../assets/images/test-pc.jpg")}
+              alt="Computer"
+              className="card__img"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Features;
--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+
 import AuthForm from "../components/Auth/AuthForm";
 
 function AuthPage() {
