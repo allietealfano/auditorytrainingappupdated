5819e7f3f78f91b8d24ebc3672220edafafc8465
jsanc446 <78328243+jsanc446@users.noreply.github.com>
2022-09-14 14:00:20 -0400

    Update Completed.js

--- src/components/Completed/Completed.js
+++ src/components/Completed/Completed.js
@@ -1,4 +1,4 @@
-import { React, useContext, useEffect } from "react";
+import { React, useContext, useEffect,useState } from "react";
 import { Link } from "react-router-dom";
 import { doc, updateDoc } from "firebase/firestore";
 
